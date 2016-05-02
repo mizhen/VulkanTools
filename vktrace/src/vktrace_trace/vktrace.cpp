7430bafb1d2bd8eb170b85625491da2c349ef3df
Peter Lohrmann <plohrmann@gmail.com>
2016-05-02 11:24:15 -0700

    vktrace: fix error when launching in server mode.

--- vktrace/src/vktrace_trace/vktrace.cpp
+++ vktrace/src/vktrace_trace/vktrace.cpp
@@ -175,6 +175,7 @@ void add_device_layer(const char* fullLayerName)
 // ------------------------------------------------------------------------------------------------
 int main(int argc, char* argv[])
 {
+    BOOL isServerMode = FALSE;
     memset(&g_settings, 0, sizeof(vktrace_settings));
 
     vktrace_LogSetCallback(loggingCallback);
@@ -231,6 +232,7 @@ int main(int argc, char* argv[])
         {
             vktrace_LogAlways("No program (-p) parameter found: Running vktrace as server.");
             g_settings.arguments = NULL;
+            isServerMode = TRUE;
         }
         else
         {
@@ -357,21 +359,23 @@ int main(int argc, char* argv[])
                 return -1;
             }
 
-            // create watchdog thread to monitor existence of remote process
-            if (g_settings.program != NULL)
-                procInfo.watchdogThread = vktrace_platform_create_thread(Process_RunWatchdogThread, &procInfo);
+            if (isServerMode == FALSE) {
+                // create watchdog thread to monitor existence of remote process
+                if (g_settings.program != NULL)
+                    procInfo.watchdogThread = vktrace_platform_create_thread(Process_RunWatchdogThread, &procInfo);
 
 #if defined(PLATFORM_LINUX)
-            // Sync wait for local threads and remote process to complete.
+                // Sync wait for local threads and remote process to complete.
 
-            vktrace_platform_sync_wait_for_thread(&(procInfo.pCaptureThreads[0].recordingThread));
+                vktrace_platform_sync_wait_for_thread(&(procInfo.pCaptureThreads[0].recordingThread));
 
-            if (g_settings.program != NULL)
-                vktrace_platform_sync_wait_for_thread(&procInfo.watchdogThread);
+                if (g_settings.program != NULL)
+                    vktrace_platform_sync_wait_for_thread(&procInfo.watchdogThread);
 #else
-            vktrace_platform_resume_thread(&procInfo.hThread);
+                vktrace_platform_resume_thread(&procInfo.hThread);
+            }
 
-            // Now into the main message loop, listen for hotkeys to send over.
+				// Now into the main message loop, listen for hotkeys to send over.
             MessageLoop();
 #endif
         }
