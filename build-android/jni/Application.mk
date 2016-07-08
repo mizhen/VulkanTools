60b579f81a5421a5357c9fca296c2446d5e6ced9
Cody Northrop <cnorthrop@google.com>
2016-07-08 08:46:56 -0600

    WIP debug stuff

--- build-android/jni/Application.mk
+++ build-android/jni/Application.mk
@@ -13,9 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-APP_ABI := armeabi-v7a arm64-v8a x86 x86_64 mips mips64
+APP_ABI := arm64-v8a # armeabi-v7a arm64-v8a x86 x86_64 mips mips64
 APP_PLATFORM := android-22
 APP_STL := gnustl_static
 APP_MODULES := layer_utils VkLayer_core_validation VkLayer_image VkLayer_parameter_validation VkLayer_object_tracker VkLayer_threading VkLayer_swapchain VkLayer_unique_objects VkLayer_api_dump VkLayerValidationTests VkLayer_vktrace_layer
-APP_CPPFLAGS += -std=c++11 -DVK_PROTOTYPES -Wall -Werror -Wno-unused-function -Wno-unused-const-variable -mxgot
+APP_CPPFLAGS += -std=c++11 -DVK_PROTOTYPES -Wall -Werror -Wno-unused-function -Wno-unused-const-variable -mxgot -g -O0
 NDK_TOOLCHAIN_VERSION := clang
