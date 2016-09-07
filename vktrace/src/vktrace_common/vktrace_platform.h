cc1d0c5ecd26ad95eb58693b41a4aa09933f4353
David Pinedo <david@lunarg.com>
2016-09-06 21:00:58 -0600

    vktrace: 32 bit trace works in Windows 7 and 10. Probably Linux too.
    Change-Id: I39d7c9059ab3b192ad7d7c6506537bab7c790f83

--- vktrace/src/vktrace_common/vktrace_platform.h
+++ vktrace/src/vktrace_common/vktrace_platform.h
@@ -76,14 +76,6 @@ typedef CRITICAL_SECTION VKTRACE_CRITICAL_SECTION;
 #include "vktrace_common.h"
 #endif
 
-#if defined(WIN32)
-    #define _CALLBACK_ CALLBACK
-#else
-    #define _CALLBACK_
-    #define _Inout_
-    #define PINIT_ONCE  void
-#endif
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -132,7 +124,11 @@ vktrace_thread vktrace_platform_create_thread(VKTRACE_THREAD_ROUTINE_RETURN_TYPE
 void vktrace_platform_resume_thread(vktrace_thread* pThread);
 void vktrace_platform_sync_wait_for_thread(vktrace_thread* pThread);
 void vktrace_platform_delete_thread(vktrace_thread* pThread);
-void vktrace_platform_thread_once(void *ctl, void (_CALLBACK_ * func) (_Inout_ PINIT_ONCE));
+#if defined(PLATFORM_LINUX)
+void vktrace_platform_thread_once(void *ctl, void (* func) (void));
+#elif defined(WIN32)
+void vktrace_platform_thread_once(void *ctl, BOOL (CALLBACK * func) (_Inout_ PINIT_ONCE initOnce, _Inout_opt_ PVOID param, _Out_opt_ PVOID *lpContext));
+#endif
 
 void vktrace_create_critical_section(VKTRACE_CRITICAL_SECTION* pCriticalSection);
 void vktrace_enter_critical_section(VKTRACE_CRITICAL_SECTION* pCriticalSection);
