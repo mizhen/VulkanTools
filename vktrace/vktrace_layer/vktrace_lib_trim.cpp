aaa8c1214ce442a95540c78eede6a5666c8b07de
mizhen <ming.zheng@amd.com>
2018-07-02 12:05:59 -0400

    vktrace: Fix error in trim 3d image handling
    There's an error in trim saving data to staging buffer if the image is
    a 3d image. It cause the trimmed trace file playback missing objects in
    image. The change fix the problem.
    
    XCAP-1019
    
    Change-Id: I0c7880a382e9982aa3e27fde9bb4025103df2bed

--- vktrace/vktrace_layer/vktrace_lib_trim.cpp
+++ vktrace/vktrace_layer/vktrace_lib_trim.cpp
@@ -1026,7 +1026,7 @@ void snapshot_state_tracker() {
                         // buffer memory size.
 
                         copyRegion.bufferOffset = layout.offset;
-                        copyRegion.imageExtent.depth = 1;
+                        copyRegion.imageExtent.depth = imageIter->second.ObjectInfo.Image.extent.depth;
                         copyRegion.imageExtent.width = imageIter->second.ObjectInfo.Image.extent.width;
                         copyRegion.imageExtent.height = imageIter->second.ObjectInfo.Image.extent.height;
                         copyRegion.imageOffset.x = 0;
@@ -1053,7 +1053,7 @@ void snapshot_state_tracker() {
                         // copy to be tightly packed according to the imageExtent.
 
                         copyRegion.bufferOffset = layout.offset;
-                        copyRegion.imageExtent.depth = 1;
+                        copyRegion.imageExtent.depth = imageIter->second.ObjectInfo.Image.extent.depth;
                         copyRegion.imageExtent.width = imageIter->second.ObjectInfo.Image.extent.width;
                         copyRegion.imageExtent.height = imageIter->second.ObjectInfo.Image.extent.height;
                         copyRegion.imageOffset.x = 0;
@@ -1083,7 +1083,7 @@ void snapshot_state_tracker() {
                         copyRegion.bufferRowLength = 0;    //< tightly packed texels
                         copyRegion.bufferImageHeight = 0;  //< tightly packed texels
                         copyRegion.bufferOffset = lay.offset;
-                        copyRegion.imageExtent.depth = 1;
+                        copyRegion.imageExtent.depth = imageIter->second.ObjectInfo.Image.extent.depth >> i;
                         copyRegion.imageExtent.width = (imageIter->second.ObjectInfo.Image.extent.width >> i);
                         copyRegion.imageExtent.height = (imageIter->second.ObjectInfo.Image.extent.height >> i);
                         copyRegion.imageOffset.x = 0;
